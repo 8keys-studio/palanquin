da8bf31b0ce652f9cadb7d10a08c30f1ffbf24c9
Christina Goefron <cagoefron@gmail.com>
2016-08-27 17:15:07 -0400

    Started Palanquin obj + script, score script, GameController obj + script.

---
+++ palanquin/Assets/gameControllerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameControllerScript : MonoBehaviour {
+
+	public int totalScore; //add to this upon completion of fare condition
+
+	public GUIText scoreText; //create GUI to display total fare - may replace this with standalone score object
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+}
---
+++ palanquin/Assets/palanquinScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class palanquinScript : MonoBehaviour {
+
+	//********NOTES**********:
+	// - May need to move some individual checks (angle, speed) to character objs, or else create bools for each condition based
+	//   on fare type
+
+	public int fareAmount; //the current/displayed fare
+	public int fareHitAmount; //the # fare is reduced by when pal takes a hit
+
+	public GUIText fareText; //create GUI to display total fare
+
+	// Use this for initialization
+	void Start () {
+		fareAmount = 10; //make this a pass through variable read by picked up fare
+		UpdateFare ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// check for angles
+		if (transform.rotation.eulerAngles.y > 90) //make # variable (change per character), confirm axis is correct. make || condition?
+			//Debug.Log(transform.eulerAngles.y); //testing
+		{
+			fareAmount = fareAmount - fareHitAmount;
+			UpdateFare ();
+		}
+
+		// check for speed
+
+	}
+
+	void OnTriggerEnter (Collider other) { //if pal hits a tagged obstacle, score is reduced
+		if (other.tag == "Obstacle")
+		{
+			fareAmount = fareAmount - fareHitAmount;
+			UpdateFare ();
+		}
+	}
+
+	void UpdateFare () //display current fare value
+	{
+		fareText.text = "Fare: " + fareAmount;
+	}
+
+
+}
+
