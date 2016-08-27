672e9cffd334dbce99d796b4e3ed1ed6a9a3d6a0
Atmajr <jratma@gmail.com>
2016-08-27 18:03:42 -0400

    - implement pause screen - turn off player collision

---
+++ palanquin/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<SpriteRenderer>().enabled = false;
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				GetComponent<SpriteRenderer>().enabled = true;
+			}
+			else
+			{
+				Time.timeScale = 1;
+				GetComponent<SpriteRenderer>().enabled = false;
+			}
+		}
+
+	}
+}
