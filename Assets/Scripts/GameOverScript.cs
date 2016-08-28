de66082daadfdbd95f7fb473b0cdb57bc82a55a5
Atmajr <jratma@gmail.com>
2016-08-28 00:20:52 -0400

    - add start menu and game over scenes - add scripts for start menu and game over - add scenes to build menu for navigating between them

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Start or quit the game
+/// 
+/// Usage: 
+/// 	-	attach this script to an empty gameobject in a GameOver scene
+/// 	-	call that scene on GameOver condition using Application.LoadLevel("GameOver");
+/// </summary>
+public class GameOverScript : MonoBehaviour
+{
+	public string retryStage = "scene1";
+	public string startMenu = "StartMenu";
+
+	void OnGUI()
+	{
+
+		int buttonWidth = new int();
+		int buttonHeight = new int();
+
+		if (((Screen.width / 12) > 120) && ((Screen.height / 10) > 60))
+		{
+			buttonWidth = Screen.width / 12;
+			buttonHeight = Screen.height / 10;
+		}
+		else
+		{
+			buttonWidth = 120;
+			buttonHeight = 60;
+		}
+		if (
+			GUI.Button(
+				// Center in X, 1/3 of the height in Y
+				new Rect(
+					Screen.width / 2 - (buttonWidth / 2),
+					(1 * Screen.height / 3) - (buttonHeight / 2),
+					buttonWidth,
+					buttonHeight
+				),
+				"Retry!"
+			)
+		)
+		{
+			// Reload the level
+			Application.LoadLevel(retryStage);
+		}
+
+		if (
+			GUI.Button(
+				// Center in X, 2/3 of the height in Y
+				new Rect(
+					Screen.width / 2 - (buttonWidth / 2),
+					(2 * Screen.height / 3) - (buttonHeight / 2),
+					buttonWidth,
+					buttonHeight
+				),
+				"Back to menu"
+			)
+		)
+		{
+			// Reload the level
+			Application.LoadLevel(startMenu);
+		}
+	}
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+/// 
+
+
+
+public class MenuScript : MonoBehaviour
+{
+
+	public string firstLevelName = "";
+
+	void OnGUI()
+	{
+
+
+		int buttonWidth = Screen.width / 12;
+		int buttonHeight = Screen.height / 10;
+
+		// Determine the button's place on screen
+		// Center in X, 2/3 of the height in Y
+		Rect buttonRect = new Rect(
+			(buttonWidth * 9),
+			(2 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+		);
+
+		// Draw a button to start the game
+		if(GUI.Button(buttonRect,"Start!"))
+		{
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel(firstLevelName);
+		}
+	}
+}
