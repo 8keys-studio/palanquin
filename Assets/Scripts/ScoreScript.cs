e461e200464d6bd1b2c5b7c64f777ef9bef6c452
Atmajr <jratma@gmail.com>
2016-08-28 04:53:36 -0400

    - add score display prefab and script

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+	private int levelScore;
+	private int totalScore;
+	private GUIText scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+
+		scoreDisplay = gameObject.GetComponent<GUIText>();
+		totalScore = GlobalControl.Instance.globalScore;
+		levelScore = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (levelScore < 1000000)
+		{
+			addToScore(11);
+		}
+		
+	}
+
+	void addToScore (int addition) {
+	
+		levelScore += addition;
+		scoreDisplay.text = "SCORE: " + levelScore;
+
+	}
+}
