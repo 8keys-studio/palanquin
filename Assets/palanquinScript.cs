b7115bd7b2849f5db271ae2fd51e5a8b097f5256
Christina Goefron <cagoefron@gmail.com>
2016-08-29 04:21:11 -0400

    Help computer. Tweaked PalScript again.

--- Assets/palanquinScript.cs
+++ Assets/palanquinScript.cs
@@ -45,6 +45,8 @@ public class palanquinScript : MonoBehaviour {
 	private Vector3 leftWheelPos;
 	private Vector3 rightWheelPos;
 
+	public int jumpForceInt;
+
 	// Use this for initialization
 	void Start () {
 		UpdateFare (); 
@@ -111,14 +113,16 @@ public class palanquinScript : MonoBehaviour {
 	
 		if (movementObj.GetComponent<movementScript>().leftJumped == true){
 				//rb.AddForce(Vector2.up * jumpspeed); //on LEFT side of Pal
-			rb.AddForceAtPosition(new Vector2(0,200), leftWheelPos, ForceMode2D.Impulse);
+			rb.velocity = new Vector2(0,1) * jumpForceInt;
+			//rb.AddForceAtPosition(new Vector2(0,jumpForceInt), leftWheelPos, ForceMode2D.Impulse); 
 			movementObj.GetComponent<movementScript> ().leftJumped = false;
 			Debug.Log ("final jump L firing" + leftWheelPos);
 		}		
 
 		if (movementObj.GetComponent<movementScript>().rightJumped == true){
 			//rb.AddForce(Vector2.up * jumpspeed); //on RIGHT side of Pal
-			rb.AddForceAtPosition(new Vector2(0,200), rightWheelPos, ForceMode2D.Impulse);
+			rb.velocity = new Vector2(0,1) * jumpForceInt;
+			//rb.AddForceAtPosition(new Vector2(0,jumpForceInt), rightWheelPos, ForceMode2D.Impulse);
 			movementObj.GetComponent<movementScript> ().rightJumped = false;
 			Debug.Log ("final jump R firing" + rightWheelPos);
 		}	
