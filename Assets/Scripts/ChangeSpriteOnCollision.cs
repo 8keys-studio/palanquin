03ce6a0edc9efe95479cf2db3ac66b0732ef8a7c
Jack Chu <jack@jackchu.com>
2016-08-28 22:45:20 -0400

    add detractor and flying tomato

---
+++ Assets/Scripts/ChangeSpriteOnCollision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSpriteOnCollision : MonoBehaviour {
+	
+	public Sprite newSprite;
+	public float destroyTime = 10.0f;
+	public float velocityX = -3.0f;
+	public float velocityY = 5.0f;
+	private bool deleting = false;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (velocityX, velocityY);
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+		spriteRenderer.sortingLayerName = "Foreground";
+		spriteRenderer.sortingOrder = 1;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter2D() {
+		if (!deleting) {
+			GetComponent<SpriteRenderer> ().sprite = newSprite;
+			deleting = true;
+			Object.Destroy (gameObject, destroyTime);
+		}
+	}
+}
---
+++ Assets/Scripts/ThrowProjectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowProjectile : MonoBehaviour
+{
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Vector3 position = new Vector3 (14.0f, 4f, 0.2f);
+		Quaternion rotation = Quaternion.Euler(45, 30, 0);
+		Instantiate(projectile, position, rotation);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
