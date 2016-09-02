724b1cd54c768aec2dbcf48c9320de49d6094fd3
Veronicats <onewingedtenshi@gmail.com>
2016-09-02 01:53:45 -0400

    Fixed goal trigger. Moved goal script to fare_kfc prefab. Fixed nubbin on back of palanquin. Made everything sticky. Theoretically you can win the game.

--- Assets/rightWheelScript.cs
+++ Assets/rightWheelScript.cs
@@ -7,15 +7,15 @@ public class rightWheelScript : MonoBehaviour {
 	public bool Rgrounded;
 	public GameObject movementObj; //attach movement script's prefab obj
 	private Rigidbody2D rb;
-	private AudioSource audio_player;
-	public AudioClip goalSound;
-	private bool firstTime = true;
+	//private AudioSource audio_player;
+	//public AudioClip goalSound;
+	//private bool firstTime = true;
 
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
-		audio_player = GetComponent<AudioSource>();
+	//	audio_player = GetComponent<AudioSource>();
 
 	}
 	
@@ -52,26 +52,26 @@ public class rightWheelScript : MonoBehaviour {
 			Rgrounded = true;
 		}
 
-		if (other.gameObject.CompareTag("goal"))
-		{
-			
-			if (firstTime){
-				Debug.Log("Goal Reached");
-				firstTime = false;
-				audio_player.PlayOneShot(goalSound, 0.7F);
-				Debug.Log("Audiowhore sucking on sound.");
-				try {
-					float fadeTime = GameObject.Find("GlobalController").GetComponent<FaderScript>().BeginFade(1);
-					Debug.Log(fadeTime);
-					Invoke("GoalReached", fadeTime);
-				}
-				catch (System.NullReferenceException ex)
-				{
-					Debug.Log(ex.Message);
-					Invoke("GoalReached", 2);
-				}
-			}
-		}
+	//	if (other.gameObject.CompareTag("goal"))
+	//	{
+	//		
+	//		if (firstTime){
+	//			Debug.Log("Goal Reached");
+	//			firstTime = false;
+	//			audio_player.PlayOneShot(goalSound, 0.7F);
+	//			Debug.Log("Audiowhore sucking on sound.");
+	//			try {
+	//				float fadeTime = GameObject.Find("GlobalController").GetComponent<FaderScript>().BeginFade(1);
+	//				Debug.Log(fadeTime);
+	//				Invoke("GoalReached", fadeTime);
+	//			}
+	//			catch (System.NullReferenceException ex)
+	//			{
+	//				Debug.Log(ex.Message);
+	//				Invoke("GoalReached", 2);
+	//			}
+	//		}
+	//	}
 
 
 	}
@@ -98,8 +98,8 @@ public class rightWheelScript : MonoBehaviour {
 
 	}
 
-	void GoalReached()
-	{
-		SceneManager.LoadScene("WinScreen", LoadSceneMode.Single);
-	}
+	//void GoalReached()
+	//{
+	//	SceneManager.LoadScene("WinScreen", LoadSceneMode.Single);
+	//}
 }
